8fef3cb42aaacd7b0f210e82ac73782e8a79202a
Robert Ris <take1risk@gmail.com>
2018-08-03 16:31:07 -0500

    add collections

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
-// model project
-var Vehicle = Backbone.Model.extend({
+// collections
+var Song = Backbone.Model.extend();
 
-	//urlRoot: “/api/vehicles”,
-
-	idAttribute: "registrationNumber",
-
-	validate: function(attrs){
-		if(!attrs.registrationNumber)
-			return "Registrations is required is required";
-	}, // end validate
-
-	start: function(){
-		console.log("Vehicle started.")	
-	}		
-
-}); // end model class	
-
-var Car = Vehicle.extend({
-	start: function(){
-		console.log("Car with registration number {registrationNumber} started.")	
-	}
+var Songs =. Backbone.Collection.extend({
+	model. Song
 });
 
-var car = new Car({registrationNumber: "XLI887", color: "blue"});
+var songs = mew Spmgs([
+		new Song({ title: "Song1"}),
+		new Song({ title: "Song2"}),
+		new Song({ title: "Song2"})
+	]);
 
+songs.add(new Song({ title: "Song 4"}));
