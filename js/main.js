a9d039f0f862b0bcc31fa3038a7ac87c4d6ca0c2
Robert Ris <take1risk@gmail.com>
2018-08-06 12:12:51 -0500

    handle collection events

--- js/main.js
+++ js/main.js
@@ -1,48 +1,40 @@
+var Song = Backbone.Model.extend();
 
-var Song = Backbone.Model.extend({
-	defaults: {
-		listeners: 0
-	}
-});
-
-/*
 var Songs =  Backbone.Collection.extend({
 	model: Song
 });
-*/
 
 // create view class
-
 var SongView = Backbone.View.extend({
-	/*
-	events: {
-		"click": "onClick",
-		"click .bookmark": "onClickBookmark",
-	},
-
-	onClick: function(){
-		console.log("Listen Clicked");
-	},
-
-	onClickBookmark: function(e){
-		e.stopPropagation();
-		console.log("Bookmark Clicked");
-	},
-	*/
-
-	initialize: function(){
-		this.model.on("change", this.render, this);
-	},
+	tagName: "li",
 
 	render: function(){
-		this.$el.html(this.model.get("title") + " - Listeners: " + this.model.get("listeners"));
+		this.$el.html(this.model.get("title"));
+		this.$el.attr("id", this.model.id);
 		return this;
 	}
 
 });
 
-/*
 var SongsView = Backbone.View.extend({
+	tagName: "ul",
+
+	initialize: function(){
+		this.model.on("add", this.onSongAdded, this);
+		this.model.on("remove", this.onSongRemoved, this);
+	},
+
+	onSongAdded: function(song){
+		var songView = new SongView({ model: song});
+		this.$el.append(songView.render().$el);
+	},
+
+	onSongRemoved: function(song){
+		//this.$el.find("li#" + song.id).remove();
+		this.$("li#" + song.id).remove();
+		console.log("Song removed");
+	},
+
 	render: function(){
 		var self = this;
 		this.model.each(function(song){
@@ -52,24 +44,15 @@ var SongsView = Backbone.View.extend({
 		});
 	}
 });
-*/
 
-/*
 var songs = new Songs([
-	new Song({ title: "Blue in Green"}),
-	new Song({ title: "So What"}),
-	new Song ({ title: "All Blues"})
+	new Song({ id: 1, title: "Blue in Green"}),
+	new Song({ id: 2, title: "So What"}),
+	new Song({ id: 3, title: "All Blues"})
 ]);
-*/
 
-/*
 var songsView = new SongsView({ el: "#songs", model: songs});
 songsView.render()
-*/
 
 
 
-var song = new Song({ title: "Blue in Green" })
-
-var songView = new SongView({ el: "#container", model: song });
-songView.render();
