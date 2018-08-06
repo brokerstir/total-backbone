68fd5bf2a302431ff4291621a613c9a54d0ab4f4
Robert Ris <take1risk@gmail.com>
2018-08-06 09:28:24 -0500

    add syntax for collections

--- js/main.js
+++ js/main.js
@@ -5,10 +5,33 @@ var Songs = Backbone.Collection.extend({
 	model: Song
 });
 
-var songs = new Songs([
-		new Song({ title: "Song1"}),
-		new Song({ title: "Song2"}),
-		new Song({ title: "Song2"})
-	]);
+var songs = new Songs()
 
-songs.add(new Song({ title: "Song 4"}));
+// specify at what index to add song to collection
+songs.add(new Song({ title: "Song 1", genre : "Jazz", downloads: 110}), { at: 0});
+
+// push methode does not allow to specify index
+songs.push(new Song({ title: "Song 2", genre : "Jazz", downloads: 90}));
+
+// where returns array
+var jazzSongs = songs.where({ genre: "Jazz" });
+
+// find where returns first instance
+var firstJazzSong = songs.findWhere({ genre: "Jazz"});
+
+console.log("Jazz Songs", jazzSongs);
+
+console.log("First Jazz Song", firstJazzSong);
+
+
+var filteredSongs = songs.where({ genre: "Jazz", title: "Song 2"});
+console.log("Filtered Songs", filteredSongs);
+
+var topDownloads = songs.filter(function(song){
+	return song.get("downloads") > 100;
+ })
+console.log("Top Downlads", topDownloads)
+
+songs.each(function(song){
+	console.log(song);
+});
