6d81c2f908fd89c3c3c92a6009740f2338201430
Robert Ris <take1risk@gmail.com>
2018-08-13 14:54:07 -0500

    create a router

--- Event Aggregator - Start/js/main.js
+++ Event Aggregator - Start/js/main.js
@@ -1,90 +1,76 @@
+var ArtistsView = Backbone.View.extend({
+	render: function(){
+		this.$el.html("ARTISTS VIEW");
 
-var Venue = Backbone.Model.extend();
-
-var Venues = Backbone.Collection.extend({
-	model: Venue
+		return this;
+	}
 });
 
-var VenueView = Backbone.View.extend({
-	tagName: "li",
+var AlbumsView = Backbone.View.extend({
+	render: function(){
+		this.$el.html("ARTISTS VIEW");
 
-	initialize: function(options){
-		this.bus = options.bus;
+		return this;
+	}	
+});
+
+var GenresView = Backbone.View.extend({
+	render: function(){
+		this.$el.html("GENRES VIEW");
+
+		return this;
+	}	
+});
+
+/////////////////////////////////////
+
+var AppRouter = Backbone.Router.extend({
+	routes: {
+		"albums": "viewAlbums",
+		"albums/:albumId": "viewAlbumId",
+		"artists": "viewArtists",
+		"genres": "viewGenres",
+		"*other": "defaultRoute"
 	},
 
+	viewArtists: function(){
+		var view = new ArtistsView({ el: "#container" });
+		view.render();
+	},
+
+	viewGenres: function(){
+		var view = new GenresView({ el: "#container" });
+		view.render();
+	},
+
+	defaultRoute: function(){
+
+	},
+
+	viewAlbmumId: function(albumId){
+
+	},
+
+	viewAlbums: function(){
+		var view = new AlbumsView({ el: "#container" });
+		view.render();
+	}
+});
+
+var router = new AppRouter();
+Backbone.history.start();
+
+
+var NavView = Backbone.View.extend({
 	events: {
-		"click": "onClick",
+		"click": "onClick"
 	},
 
-	onClick: function(){
-		this.bus.trigger("venueSelected", this.model);
-	},
-
-	render: function(){
-		this.$el.html(this.model.get("name"));
-
-		return this;
+	onClick: function(e){
+		var $li = $(e.target);
+		router.navigate($li.attr("data-url"), { trigger: true });
 	}
+
 });
 
-var VenuesView = Backbone.View.extend({
-	tagName: "ul",
-
-	id: "venues",
-
-	initialize: function(options){
-		this.bus = options.bus;
-	},
-
-	render: function(){
-		var self = this;
-
-		this.model.each(function(venue){
-			var view = new VenueView({ model: venue, bus: self.bus });
-			self.$el.append(view.render().$el);
-		});
-
-		return this;
-	}
-});
-
-var MapView = Backbone.View.extend({
-	el: "#map-container",
-
-	initialize: function(options){
-		this.bus = options.bus;
-
-		this.bus.on("venueSelected", this.onVenueSelected, this);
-	},
-
-	onVenueSelected: function(venue){
-		this.model = venue;
-		this.render();
-	},
-
-	render: function(){
-		if (this.model)
-			this.$("#venue-name").html(this.model.get("name"));
-
-		return this;
-	}
-})
-
-var bus = _.extend({}, Backbone.Events);
-
-var venues = new Venues([
-	new Venue({ name: "30 Mill Espresso" }),
-	new Venue({ name: "Platform Espresso" }),
-	new Venue({ name: "Mr Foxx" })
-	]);
-
-var venuesView = new VenuesView({ model: venues, bus: bus});
-$("#venues-container").html(venuesView.render().$el);
-
-var mapView = new MapView({ bus: bus });
-mapView.render();
-
-
-
-
-
+var navView = new NavView({ el: "#nav" });
