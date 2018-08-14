49bf416396d0f07224b490b380d1f28a9d89c33d
Robert Ris <take1risk@gmail.com>
2018-08-14 11:40:32 -0500

    add input to add to do item

--- Todo-Basics-Start/js/todoitemsview.js
+++ Todo-Basics-Start/js/todoitemsview.js
@@ -7,11 +7,32 @@ var TodoItemsView = Backbone.View.extend({
 	initialize: function(options){
 		if (!(options && options.model))
 			throw new Error("model is not specified.");
+
+		this.model.on("add", this.onAddTodoItem, this);
+	},
+
+	onAddTodoItem: function(todoItem){
+		//console.log("Added");
+		var view = new TodoItemView({ model: todoItem });
+		this.$el.append(view.render().$el);
+	},
+
+	events: {
+		"click #add": "onClickAdd"
+	},
+
+	onClickAdd: function(){
+		//console.log("Clicked");
+		var todoItem = new TodoItem({ description: this.$("#newTodoItem").val() });
+		this.model.add(todoItem);
 	},
 
 	render: function(){
 
 		var self = this;
+
+		this.$el.append("<input type='text' id='newTodoItem'></input>");
+		this.$el.append("<button id='add'>Add</button>");
 
 		this.model.each(function(todoItem){
 			var view = new TodoItemView({ model: todoItem });
